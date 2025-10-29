be68a1a74f79ef6ff95c0930565b84f0c7033d7f
vipin <mjvipinms@gmail.com>
2025-10-29 17:58:06 +0530

    notification changes

--- src/main/java/com/ibs/notification_service/service/EmailNotificationService.java
+++ src/main/java/com/ibs/notification_service/service/EmailNotificationService.java
@@ -27,13 +27,19 @@ public class EmailNotificationService {
      */
     public void sendInterviewCreatedMail(InterviewCreatedEvent event) {
         sendMail(event.getCandidateEmail(), "Interview Scheduled",
-                "Dear Candidate,<br>Your interview is scheduled for <b>" + event.getStartTime() + "</b>.",event.getCreatedBy());
+                "Dear Candidate,<br>Your interview is scheduled for <b>" + event.getStartTime()  +
+                        "<br>Thanks & Regards," +
+                        "<br>IBS Talent Team", event.getCreatedBy());
 
         sendMail(event.getPanelEmail(), "Interview Assigned",
-                "Dear Panel,<br>You have a new interview scheduled for <b>" + event.getStartTime() + "</b>.", event.getCreatedBy());
+                "Dear Panel,<br>You have a new interview scheduled for <b>" + event.getStartTime()  +
+                        "<br>Thanks & Regards," +
+                        "<br>IBS Talent Team", event.getCreatedBy());
 
         sendMail(event.getHrEmail(), "Interview Confirmation",
-                "Dear HR,<br>An interview has been created for candidate associated with ID: " + event.getInterviewId(), event.getCreatedBy());
+                "Dear HR,<br>An interview has been created for candidate associated with ID: " + event.getInterviewId() +
+                        "<br>Thanks & Regards," +
+                        "<br>IBS Talent Team", event.getCreatedBy());
     }
 
     /**
@@ -62,14 +68,14 @@ public class EmailNotificationService {
 
     /**
      *
-     * @param to recipient email
-     * @param subject subject of the mail
+     * @param to          recipient email
+     * @param subject     subject of the mail
      * @param htmlContent content
      */
     private void sendMail(String to, String subject, String htmlContent, String createdBy) {
         try {
 
-            if(isValidEmail(to) && !to.contains("test")){
+            if (isValidEmail(to) && !to.contains("test")) {
                 MimeMessage message = mailSender.createMimeMessage();
                 MimeMessageHelper helper = new MimeMessageHelper(message, true);
                 helper.setTo(to);
@@ -90,6 +96,7 @@ public class EmailNotificationService {
             throw new RuntimeException("Failed to send mail to " + to, e);
         }
     }
+
     private static final Pattern EMAIL_PATTERN = Pattern.compile(
             "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$"
     );
